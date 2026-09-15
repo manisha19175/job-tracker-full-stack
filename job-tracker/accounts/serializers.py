40b5955c1794b7c162a5f8dd95beb76083b5d93f
Manisha <manishapenjarla431@gmail.com>
2026-09-15 04:21:10 -0400

    Fix optional email during registration

--- job-tracker/accounts/serializers.py
+++ job-tracker/accounts/serializers.py
@@ -3,18 +3,21 @@ from django.contrib.auth.models import User
 
 
 class RegisterSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = User
         fields = ["username", "email", "password"]
 
         extra_kwargs = {
-            "password": {"write_only": True}
+            "password": {"write_only": True},
+            "email": {"required": False},
         }
 
     def create(self, validated_data):
+
         user = User.objects.create_user(
             username=validated_data["username"],
-            email=validated_data["email"],
+            email=validated_data.get("email", ""),
             password=validated_data["password"]
         )
 
